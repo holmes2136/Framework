e1935df7da4af54513d0933721de7065ba345772
Holmes J. <holmes2136@gmail.com>
2015-02-09 22:03:21 +0800

    update

--- App_Code/DataAccessLayer/DataAccess.cs
+++ App_Code/DataAccessLayer/DataAccess.cs
@@ -5,16 +5,190 @@ using System.Web;
 using System.Data.Common;
 using System.Collections;
 using Utilities;
+using System.Data;
 
 namespace DataAccessLayer { 
   
 
     public class DataAccess
     {
+        private static IPerRequestDataStorage _transactionStorage = new HttpContextPerRequestDataStorage();
+
+        private static DbTransaction Transaction
+        {
+            get
+            {
+                if (_transactionStorage.Items.Contains("DbTransaction"))
+                    return (DbTransaction)_transactionStorage.Items["DbTransaction"];
+                else
+                    return null;
+            }
+            set
+            {
+                _transactionStorage.Items["DbTransaction"] = value;
+            }
+        }
+
 	    public DataAccess()
 	    {
 		
 	    }
+
+        public static DataTable ExecuteSelect(string sqlString)
+        {
+            DbCommand dbCommand = DataAccess.CreateTextCommand();
+            dbCommand.CommandText = sqlString;
+            return DataAccess.ExecuteSelect(dbCommand);
+        }
+
+        public static DbCommand CreateTextCommand()
+        {
+            return DataAccess.CreateCommand();
+        }
+
+        public static DbParameter CreateParameterString(string value)
+        {
+            DbParameter dbParameter = DataAccess.DbFactory.CreateParameter();
+            dbParameter.DbType = DbType.String;
+            dbParameter.Value = value;
+            return dbParameter;
+        }
+
+
+        public static DataTable ExecuteSelect(DbCommand command, params DbParameter[] parameters)
+        {
+            DataTable result;
+            try
+            {
+                if (DataAccess.Transaction == null)
+                {
+                    result = DataAccess.ExecuteSelectNoTransaction(command, parameters);
+                }
+                else
+                {
+                    result = DataAccess.ExecuteSelectWithTransaction(command, parameters);
+                }
+            }
+            catch (Exception ex)
+            {
+                //if (DataAccessHelper.ShowVerboseError)
+                //{
+                //    throw DataAccess.CreateVerboseException(ex, command, parameters);
+                //}
+                throw ex;
+            }
+            return result;
+        }
+
+        public static DataTable ExecuteSelectWithTransaction(DbCommand command, params DbParameter[] parameters)
+        {
+            command.Transaction = DataAccess.Transaction;
+            return DataAccess.ExecuteSelectImplementation(command, parameters);
+        }
+
+        public static DataTable ExecuteSelectNoTransaction(DbCommand command, params DbParameter[] parameters)
+        {
+            DataTable result = null;
+            try
+            {
+                command.Connection.Open();
+                result = DataAccess.ExecuteSelectImplementation(command, parameters);
+            }
+            finally
+            {
+                command.Connection.Close();
+            }
+            return result;
+        }
+
+        public static DataTable ExecuteSelectImplementation(DbCommand command, params DbParameter[] parameters)
+        {
+            DataTable dataTable;
+            try
+            {
+                DbCommand[] array = DataAccess.SplitBatchCommand(command, parameters);
+                DbDataReader dbDataReader = null;
+                DbCommand[] array2 = array;
+                for (int i = 0; i < array2.Length; i++)
+                {
+                    DbCommand dbCommand = array2[i];
+                    if (dbDataReader != null)
+                    {
+                        dbDataReader.Close();
+                    }
+                    dbCommand.CommandTimeout = 300;
+                    dbDataReader = dbCommand.ExecuteReader();
+                }
+                dataTable = new DataTable();
+                dataTable.Load(dbDataReader);
+                dbDataReader.Close();
+            }
+            catch (Exception ex)
+            {
+                //if (DataAccessHelper.LogError != null)
+                //{
+                //    DataAccessHelper.LogError(ex);
+                //}
+                throw ex;
+            }
+            return dataTable;
+        }
+
+
+        public static int ExecuteNonQueryNoParameter(DbCommand command)
+        {
+            if (Transaction == null)
+                return ExecuteNonQueryNoParameterNoTransaction(command);
+            else
+                return ExecuteNonQueryNoParameterWithTransaction(command);
+        }
+
+        private static int ExecuteNonQueryNoParameterWithTransaction(DbCommand command)
+        {
+            command.Transaction = Transaction;
+            return ExecuteNonQueryNoParameterImplementation(command);
+        }
+
+
+        // ExecuteNonQueryNoParameter-Related Functions
+        private static int ExecuteNonQueryNoParameterImplementation(DbCommand command)
+        {
+            int affectedRows = -1;
+            string commandTextLine = String.Empty;
+            try
+            {
+                DbCommand[] commandList = SplitBatchCommandNoParameter(command);
+
+                foreach (DbCommand current in commandList)
+                {
+                    commandTextLine = current.CommandText;
+                    current.CommandTimeout = 300;
+                    affectedRows = current.ExecuteNonQuery();
+                }
+            }
+            catch (Exception ex)
+            {
+                //DataAccessHelper.LogError(ex);
+                throw new Exception(ex.Message + "<br/> Command :" + commandTextLine);
+            }
+            return affectedRows;
+        }
+
+        private static int ExecuteNonQueryNoParameterNoTransaction(DbCommand command)
+        {
+            int affectedRows = -1;
+            try
+            {
+                command.Connection.Open();
+                affectedRows = ExecuteNonQueryNoParameterImplementation(command);
+            }
+            finally
+            {
+                command.Connection.Close();
+            }
+
+            return affectedRows;
+        }
 
         private static DbCommand[] SplitBatchCommandNoParameter(DbCommand command)
         {
--- Sample.aspx.cs
+++ Sample.aspx.cs
@@ -10,13 +10,19 @@ using System.Web.UI.HtmlControls;
 using System.Web.UI.WebControls;
 using System.Web.UI.WebControls.WebParts;
 using System.Xml.Linq;
-
+using System.Data.Common;
+using System.Data.SqlClient;
+using DataAccessLayer;
 
 public partial class Sample : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
 
+
+        SqlConnection connection = new SqlConnection("connectionString");
+        DbCommand command = new SqlCommand("commandText", connection);
+        DataAccess.ExecuteNonQueryNoParameter(command);
     }
 
     
